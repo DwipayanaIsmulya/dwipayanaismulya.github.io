8bebc3d2010fb6f4dcf71576470fcadd5f117f06
Dwipayana Ismulya <dwipayanaismulya6@gmail.com>
2020-11-28 11:54:19 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// // Scroll On Nav
+// $(window).on('scroll', function() {
+//     if  ($(window).scrollTop()) {
+//         $('.theTop').addClass('advance');
+//     } else {
+//         $('.theTop').removeClass('advance');
+//     }
+// })
+
+
+
+
+
+// Event pada saat link di klik
+// $('.page-scroll').on('click', function(e) {
+
+//     // Ambil isi href
+//     var href = $(this).attr('href');
+//     // tangkap element yang bersangkutan
+//     var elementHref = $(href);
+    
+//     // pindahkan scroll
+
+//     console.log($('body').scrollTop());
+
+//     e.preventDefault();
+
+// });
+
